d5843f7c9cb12bc648da730b80da239be8656abe
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-20 14:13:47 +0100

    snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,17 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+
+//utilizzo filter per selezionare elementi di un array che rispettino una condizione 
+const evenNumb = nums.filter(function(numero){
+    if(numero % 2 === 0){
+        return true
+    }
+})
+console.log(evenNumb);
+
+//in forma sintetica
+const numeriPari = nums.filter(numero => numero % 2 === 0);
+
+console.log(numeriPari);
