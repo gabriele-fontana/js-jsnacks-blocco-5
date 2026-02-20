667dd943b3c279af957ecb8ea72f26a4ec58b01a
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-20 13:57:33 +0100

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,23 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+//utilizzo map per stampare tutti i nomi
+//utiizzo join per scriverli in un unica riga
+console.log(names.map(name => `'${name}'`).join(', '));
+
+
+//posso usare anche forEach (ma stampa la virgola dopo ogni nome)
+let result = '';
+
+names.forEach((name, index) => {
+
+  result += `'${name}',`;
+  
+});
+console.log(result);
+
+
+//posso loggare "manualmente" ogni nome (se aggiungo nomi all'array non verranno stampati)
+console.log(`'${names[0]}', '${names[1]}', '${names[2]}'`);
