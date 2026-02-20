b9ccb0b89f6962115e60f77acd177c5b01bd4d54
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-20 14:09:44 +0100

    snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+newArr = posts.map(post => post.author)
+
+console.log(newArr);
