ee401876311445d47f1403ff1df694cda8bfd165
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-20 14:30:29 +0100

    snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//utilizzo find per selezionare un oggetto mediante una chiave
+
+console.log(students.find(student => {
+  return student.id === 2
+}))
